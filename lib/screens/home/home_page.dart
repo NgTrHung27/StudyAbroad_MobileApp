adbf9a73e877582f77e34271c5a08a92f126665d
Nguyễn Trung Hưng <95417445+NgTrHung27@users.noreply.github.com>
2024-07-08 19:54:56 +0700

    update floating

--- lib/screens/home/home_page.dart
+++ lib/screens/home/home_page.dart
@@ -16,8 +16,7 @@ import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:kltn_mobile/models/news.dart';
 import 'package:kltn_mobile/models/user_login.dart';
 import 'package:kltn_mobile/screens/home/base_lang.dart';
-import 'package:kltn_mobile/screens/home/dismissible_bottom_sheet_view.dart';
-
+import 'package:kltn_mobile/screens/chatting/dismissible_bottom_sheet_view.dart';
 
 class HomePage extends BasePage {
   const HomePage({super.key, UserAuthLogin? userAuth, NewsList? newsData});
@@ -51,111 +50,104 @@ class _HomePageState extends BasePageState<HomePage> {
         (ThemeSettingCubit cubit) => cubit.state.brightness == Brightness.dark);
     final textColorRed = isDarkMode ? Colors.white : AppColor.redButton;
     return Scaffold(
-      backgroundColor: context.select(
-          (ThemeSettingCubit cubit) => cubit.state.scaffoldBackgroundColor),
-      body: BlocBuilder<LanguageBloc, Locale>(
-        builder: (context, state) {
-          return Padding(
-            padding: EdgeInsets.symmetric(
-                horizontal: screenWidth * 0.04, vertical: screenHeight * 0.01),
-            child: ListView(
-              children: [
-                WelcomeAVT(username: userAuth?.name ?? 'N/A'),
-                SizedBox(height: screenHeight * 0.01),
-                const NewsSearchTextField(),
-                SizedBox(height: screenHeight * 0.02),
-                BlocBuilder<CarouselBloc, CarouselState>(
-                  builder: (context, state) {
-                    if (state is CarouselLoading) {
-                      return const CarouselLoadingCustom();
-                    } else if (state is CarouselLoaded) {
-                      return CarouselSliderDataFound(state.carousels);
-                    } else if (state is CarouselError) {
-                      return Center(child: Text(state.message));
-                    } else {
-                      return Container();
-                    }
-                  },
-                ),
-                TextMonserats(
-                  homeActionText,
-                  fontSize: 20,
-                  fontWeight: FontWeight.w700,
-                  color: textColorRed,
-                ),
-                SizedBox(height: screenHeight * 0.01),
-                const BoxGridView(),
-                SizedBox(height: screenHeight * 0.01),
-                TextMonserats(
-                  homeExploreText,
-                  fontSize: 20,
-                  fontWeight: FontWeight.w700,
-                  color: textColorRed,
-                ),
-                SizedBox(height: screenHeight * 0.01),
-                SizedBox(
-                  height: 200,
+        backgroundColor: context.select(
+            (ThemeSettingCubit cubit) => cubit.state.scaffoldBackgroundColor),
+        body: Stack(
+          children: [
+            BlocBuilder<LanguageBloc, Locale>(
+              builder: (context, state) {
+                return Padding(
+                  padding: EdgeInsets.symmetric(
+                      horizontal: screenWidth * 0.04,
+                      vertical: screenHeight * 0.01),
                   child: ListView(
-                    scrollDirection: Axis.horizontal,
                     children: [
-                      GestureDetector(
-                        onTap: () {
-                          Navigator.of(context).pushNamed('/schoolslistCanada');
+                      WelcomeAVT(username: userAuth?.name ?? 'N/A'),
+                      SizedBox(height: screenHeight * 0.01),
+                      const NewsSearchTextField(),
+                      SizedBox(height: screenHeight * 0.02),
+                      BlocBuilder<CarouselBloc, CarouselState>(
+                        builder: (context, state) {
+                          if (state is CarouselLoading) {
+                            return const CarouselLoadingCustom();
+                          } else if (state is CarouselLoaded) {
+                            return CarouselSliderDataFound(state.carousels);
+                          } else if (state is CarouselError) {
+                            return Center(child: Text(state.message));
+                          } else {
+                            return Container();
+                          }
                         },
-                        child: SizedBox(
-                            width: 330,
-                            child: Image.asset('assets/countries/Canada.png')),
                       ),
-                      SizedBox(width: screenWidth * 0.02),
-                      GestureDetector(
-                        onTap: () {
-                          Navigator.of(context).pushNamed('/schoolslistCanada');
-                        },
-                        child: SizedBox(
-                            width: 330,
-                            child:
-                                Image.asset('assets/countries/Australia.png')),
+                      TextMonserats(
+                        homeActionText,
+                        fontSize: 20,
+                        fontWeight: FontWeight.w700,
+                        color: textColorRed,
                       ),
-                      SizedBox(width: screenWidth * 0.02),
-                      GestureDetector(
-                        onTap: () {
-                          Navigator.of(context).pushNamed('/schoolslistCanada');
-                        },
-                        child: SizedBox(
-                            width: 330,
-                            child: Image.asset('assets/countries/Korea.png')),
+                      SizedBox(height: screenHeight * 0.01),
+                      const BoxGridView(),
+                      SizedBox(height: screenHeight * 0.01),
+                      TextMonserats(
+                        homeExploreText,
+                        fontSize: 20,
+                        fontWeight: FontWeight.w700,
+                        color: textColorRed,
                       ),
+                      SizedBox(height: screenHeight * 0.01),
+                      SizedBox(
+                        height: 200,
+                        child: ListView(
+                          scrollDirection: Axis.horizontal,
+                          children: [
+                            GestureDetector(
+                              onTap: () {
+                                Navigator.of(context)
+                                    .pushNamed('/schoolslistCanada');
+                              },
+                              child: SizedBox(
+                                  width: 330,
+                                  child: Image.asset(
+                                      'assets/countries/Canada.png')),
+                            ),
+                            SizedBox(width: screenWidth * 0.02),
+                            GestureDetector(
+                              onTap: () {
+                                Navigator.of(context)
+                                    .pushNamed('/schoolslistCanada');
+                              },
+                              child: SizedBox(
+                                  width: 330,
+                                  child: Image.asset(
+                                      'assets/countries/Australia.png')),
+                            ),
+                            SizedBox(width: screenWidth * 0.02),
+                            GestureDetector(
+                              onTap: () {
+                                Navigator.of(context)
+                                    .pushNamed('/schoolslistCanada');
+                              },
+                              child: SizedBox(
+                                  width: 330,
+                                  child: Image.asset(
+                                      'assets/countries/Korea.png')),
+                            ),
+                          ],
+                        ), // Add the image here
+                      ),
+                      SizedBox(height: screenHeight * 0.02),
+                      TextMonserats(homeNewListText,
+                          fontSize: 20, fontWeight: FontWeight.w700),
+                      SizedBox(height: screenHeight * 0.02),
+                      const NewsListViewShort(),
+                      SizedBox(height: screenHeight * 0.14),
                     ],
-                  ), // Add the image here
-                ),
-                SizedBox(height: screenHeight * 0.02),
-                TextMonserats(homeNewListText,
-                    fontSize: 20, fontWeight: FontWeight.w700),
-                SizedBox(height: screenHeight * 0.02),
-                const NewsListViewShort(),
-                SizedBox(height: screenHeight * 0.14),
-              ],
+                  ),
+                );
+              },
             ),
-          );
-        },
-      ),
-      floatingActionButton: FloatingChatButton(
-        onTap: (_) {
-          _showBottomSheet(context);
-        },
-        chatIconWidget: const Padding(
-          padding: EdgeInsets.all(14.0),
-          child: Icon(
-            Icons.perm_identity,
-            color: Colors.green,
-            size: 40,
-          ),
-        ),
-        messageBackgroundColor: Colors.green,
-        chatIconBorderColor: Colors.green,
-        chatIconBackgroundColor: Colors.white,
-      ),
-    );
+          ],
+        ));
   }
 
   Future<void> _showBottomSheet(BuildContext bContext) async {
--- lib/screens/home/mainpage.dart
+++ lib/screens/home/mainpage.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:kltn_mobile/components/constant/color_constant.dart';
 import 'package:kltn_mobile/components/functions/alert_dialog.dart';
 import 'package:kltn_mobile/components/navbar/bottom_navbar.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:kltn_mobile/screens/authentication/auth_notify.dart';
+import 'package:kltn_mobile/screens/chatting/floating_chatting_position.dart';
 import 'package:provider/provider.dart';
+import 'package:kltn_mobile/screens/chatting/dismissible_bottom_sheet_view.dart';
+import 'package:floating_chat_button/floating_chat_button.dart';
 
 import 'home_page.dart';
 import '../notifications/notifications_page.dart';
@@ -37,61 +41,104 @@ class MainPageState extends State<MainPage> {
     final localizations = AppLocalizations.of(context);
     final home =
         localizations != null ? localizations.nav_home : 'Default Text';
+    final chatAI =
+        localizations != null ? localizations.ai_chatting_nav : 'Default Text';
     final noti =
         localizations != null ? localizations.nav_noti : 'Default Text';
     final profile =
         localizations != null ? localizations.nav_profile : 'Default Text';
 
-    return Scaffold(
-      body: Stack(
-        children: [
-          IndexedStack(
+    return Stack(
+      children: [
+        Scaffold(
+          body: IndexedStack(
             index: _currentIndex,
             children: _children,
           ),
-          Positioned(
-            bottom: 15,
-            left: 0,
-            right: 0,
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: BottomNavbar(
-                items: [
-                  BottomNavbarItem(
-                      icon: 'assets/iconHome',
-                      label: home,
-                      onTap: () => onTabTapped(0)),
-                  BottomNavbarItem(
-                      icon: 'assets/iconMess',
-                      label: 'Chat AI',
-                      onTap: () {
-                        Navigator.pushNamed(context, '/chatting');
-                      }),
-                  BottomNavbarItem(
-                    icon: 'assets/iconNoti',
-                    label: noti,
-                    // onTap: () => onTabTapped(1)),
-                    onTap: () {
-                      isLoggedIn
-                          ? onTabTapped(1)
-                          : showCustomDialog(
-                              context: context,
-                              onConfirm: () {
-                                Navigator.pushNamed(context, '/login');
-                              },
-                            );
-                    },
-                  ),
-                  BottomNavbarItem(
-                      icon: 'assets/iconUser',
-                      label: profile,
-                      onTap: () => onTabTapped(2)),
-                ],
-                initialIndex: _currentIndex,
+          floatingActionButton: FloatingChatButton(
+            onTap: (_) {
+              _showBottomSheet(context);
+            },
+            chatIconWidget: Padding(
+              padding: const EdgeInsets.all(14.0),
+              child: Icon(
+                Icons.contact_support_outlined,
+                color: AppColor.redButton,
+                size: 40,
               ),
             ),
+            messageBackgroundColor: AppColor.redButton,
+            chatIconBorderColor: AppColor.redButton,
+            chatIconBackgroundColor: Colors.white,
           ),
-        ],
+          floatingActionButtonLocation: CustomFloatingActionButtonLocation(
+            verticalOffset: 70,
+            horizontalOffset: 10,
+          ),
+        ),
+        Positioned(
+          bottom: 15,
+          left: 0,
+          right: 0,
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: BottomNavbar(
+              items: [
+                BottomNavbarItem(
+                    icon: 'assets/iconHome',
+                    label: home,
+                    onTap: () => onTabTapped(0)),
+                BottomNavbarItem(
+                    icon: 'assets/iconMess',
+                    label: chatAI,
+                    onTap: () {
+                      Navigator.pushNamed(context, '/chatting');
+                    }),
+                BottomNavbarItem(
+                  icon: 'assets/iconNoti',
+                  label: noti,
+                  // onTap: () => onTabTapped(1)),
+                  onTap: () {
+                    isLoggedIn
+                        ? onTabTapped(1)
+                        : showCustomDialog(
+                            context: context,
+                            onConfirm: () {
+                              Navigator.pushNamed(context, '/login');
+                            },
+                          );
+                  },
+                ),
+                BottomNavbarItem(
+                    icon: 'assets/iconUser',
+                    label: profile,
+                    onTap: () => onTabTapped(2)),
+              ],
+              initialIndex: _currentIndex,
+            ),
+          ),
+        )
+      ],
+    );
+  }
+
+  Future<void> _showBottomSheet(BuildContext bContext) async {
+    return showModalBottomSheet(
+      enableDrag: false,
+      isScrollControlled: true,
+      backgroundColor: Colors.transparent,
+      context: bContext,
+      builder: (context) => DismissibleBottomSheetView(
+        childView: Container(
+            width: double.infinity,
+            color: Colors.white,
+            child: const Center(
+              child: Padding(
+                padding: EdgeInsets.all(20.0),
+                child: Text("Imagine this is a chat UI",
+                    style: TextStyle(fontSize: 30, color: Colors.blue)),
+              ),
+            )),
       ),
     );
   }
