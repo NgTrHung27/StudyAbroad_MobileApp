0aa294b2352fa004b8d662b535685ac34ff4fa33
felixquang <tranquang071203@gmail.com>
2024-07-26 17:23:39 +0700

    fix compare schools

--- lib/components/functions/schoolcomparisonbox.dart
+++ lib/components/functions/schoolcomparisonbox.dart
@@ -23,113 +23,124 @@ class ComparisonChart extends StatelessWidget {
         (ThemeSettingCubit cubit) => cubit.state.brightness == Brightness.dark);
     final textColor = isDarkMode ? Colors.white : AppColor.redButton;
 
-    return Column(
-      children: [
-        Container(
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(10),
-            border: Border.all(
-              color: Colors.grey[600]!,
-              width: 1,
+    return Padding(
+      padding: EdgeInsets.only(bottom: screenHeight * 0.05),
+      child: Column(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              border: Border.all(
+                color: Colors.grey[600]!,
+                width: 1,
+              ),
+            ),
+            child: Column(
+              children: [
+                SizedBox(height: screenHeight * 0.02),
+                TextMonserats(
+                  titleComparison,
+                  color: textColor,
+                  fontSize: screenWidth * 0.045,
+                  fontWeight: FontWeight.w600,
+                ),
+                Transform(
+                  transform:
+                      Matrix4.translationValues(0.0, screenHeight * 0.065, 0.0),
+                  child: SingleChildScrollView(
+                    scrollDirection: Axis.horizontal,
+                    child: Column(
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: schoolNames.map((name) {
+                            final double percentage =
+                                Random().nextDouble() * 100;
+                            return Column(
+                              children: [
+                                Container(
+                                  width: screenWidth * 0.25,
+                                  margin: EdgeInsets.symmetric(
+                                      horizontal: screenWidth * 0.02),
+                                  constraints: const BoxConstraints(
+                                      minHeight: 80, maxHeight: 200),
+                                  child: Stack(
+                                    alignment: Alignment.bottomCenter,
+                                    children: [
+                                      Container(
+                                        width: screenWidth * 0.15,
+                                        height: screenHeight * 0.24,
+                                        decoration: const BoxDecoration(
+                                          borderRadius: BorderRadius.only(
+                                            topLeft: Radius.circular(10),
+                                            topRight: Radius.circular(10),
+                                          ),
+                                        ),
+                                      ),
+                                      Positioned(
+                                        bottom: 0,
+                                        child: Container(
+                                          width: screenWidth * 0.15,
+                                          height: percentage * 2.0,
+                                          decoration: const BoxDecoration(
+                                            gradient: LinearGradient(
+                                              colors: [
+                                                Color(0xffF58218),
+                                                Color(0xff9C2424),
+                                              ],
+                                              begin: Alignment.topCenter,
+                                              end: Alignment.bottomCenter,
+                                            ),
+                                            borderRadius: BorderRadius.vertical(
+                                                top: Radius.circular(20)),
+                                          ),
+                                        ),
+                                      ),
+                                      Positioned(
+                                        bottom: 10,
+                                        child: Text(
+                                          '${percentage.toStringAsFixed(0)}%',
+                                          style: TextStyle(
+                                            color: Colors.white,
+                                            fontWeight: FontWeight.bold,
+                                            fontSize: screenWidth * 0.04,
+                                          ),
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ],
+                            );
+                          }).toList(),
+                        ),
+                        SizedBox(height: screenHeight * 0.01),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: schoolNames.map((name) {
+                            return SizedBox(
+                              width: screenWidth * 0.3,
+                              child: TextMonserats(
+                                name,
+                                color: textColor,
+                                fontSize: screenWidth * 0.033,
+                                fontWeight: FontWeight.w600,
+                                maxLine: 3,
+                                height: 1.2,
+                                textAlign: TextAlign.center,
+                              ),
+                            );
+                          }).toList(),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
             ),
           ),
-          child: Column(
-            children: [
-              const SizedBox(
-                height: 10,
-              ),
-              TextMonserats(
-                titleComparison,
-                color: textColor,
-                fontSize: screenWidth * 0.045,
-                fontWeight: FontWeight.w600,
-              ),
-              const SizedBox(height: 20),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: schoolNames.map((name) {
-                  final double percentage = Random().nextDouble() * 100;
-                  return Column(
-                    children: [
-                      Container(
-                        width: screenWidth * 0.25,
-                        margin: EdgeInsets.symmetric(
-                            horizontal: screenWidth * 0.02),
-                        constraints: const BoxConstraints(
-                          minHeight: 80,
-                        ),
-                        child: Stack(
-                          alignment: Alignment.bottomCenter,
-                          children: [
-                            Container(
-                              width: screenWidth * 0.15,
-                              height: 200,
-                              decoration: const BoxDecoration(
-                                borderRadius: BorderRadius.only(
-                                  topLeft: Radius.circular(10),
-                                  topRight: Radius.circular(10),
-                                ),
-                              ),
-                            ),
-                            Positioned(
-                              bottom: 0,
-                              child: Container(
-                                width: screenWidth * 0.15,
-                                height: percentage * 2.0,
-                                decoration: const BoxDecoration(
-                                  gradient: LinearGradient(
-                                    colors: [
-                                      Color(0xffF58218),
-                                      Color(0xff9C2424),
-                                    ],
-                                    begin: Alignment.topCenter,
-                                    end: Alignment.bottomCenter,
-                                  ),
-                                  borderRadius: BorderRadius.vertical(
-                                      top: Radius.circular(20)),
-                                ),
-                              ),
-                            ),
-                            Positioned(
-                              bottom: 10,
-                              child: Text(
-                                '${percentage.toStringAsFixed(0)}%',
-                                style: TextStyle(
-                                  color: Colors.white,
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: screenWidth * 0.04,
-                                ),
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ],
-                  );
-                }).toList(),
-              ),
-            ],
-          ),
-        ),
-        SizedBox(height: screenHeight * 0.01),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: schoolNames.map((name) {
-            return SizedBox(
-              width: screenWidth * 0.3,
-              child: TextMonserats(
-                name,
-                color: textColor,
-                fontSize: screenWidth * 0.033,
-                fontWeight: FontWeight.w600,
-                maxLine: 3,
-                height: 1.2,
-                textAlign: TextAlign.center,
-              ),
-            );
-          }).toList(),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
